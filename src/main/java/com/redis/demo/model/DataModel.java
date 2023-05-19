7f7eee8845c84a42d16c1b629e66417c3409f877
Numero Uno <numerounodeveloper@gmail.com>
2023-05-19 16:48:11 -0400

    Initial commit

---
+++ src/main/java/com/redis/demo/model/DataModel.java
@@ -0,0 +1,16 @@
+package com.redis.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataModel {
+
+    private String key;
+    private String value;
+
+
+}
